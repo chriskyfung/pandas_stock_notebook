cfb6727676024dbda8e876e1e5372e4ebfc1f823
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2021-07-05 09:59:49 +0800

    Add comments to stocks.py

--- stocks.py
+++ stocks.py
@@ -5,43 +5,46 @@ import pandas_datareader as pdr
 
 class stock_profile:
   def __init__(self, fpath=None):
-    self.listing = {}
-    self.name_dict = {}
-    self.dataframes = {}
+    self.listing = {} # A dict for storing the key-value pairs of stock codes and their preferred name
+    self.name_dict = {} # A dict for storing the key-value pairs of stock names/aliases and their corresponding code
+    self.dataframes = {} # A dict for mapping stock codes and their associated dataframes
     if fpath:
-      self.loadJsonProfile(fpath)
+      self.loadJsonProfile(fpath) # Load profile data from the specified JSON file if the arg `fpath` is passed in the initialization
     return
 
+  # Read the codes and names of a basket of stocks from a JSON-formatted file and map them to the `listing` and `name_dict` dictionary objects
   def loadJsonProfile(self, fpath):
     with open(fpath) as f:
       jsondata = json.load(f)
     self.listing = dict((k.lower(), v.split(',',1)[0]) for k, v in jsondata['listing'].items())
-    # self.name_dict = dict((k, v.lower()) for k, v in jsondata['name_dict'].items())
     for k, v in self.listing.items():
       names = v.split(',')
       for n in names:
         self.name_dict[n.strip()] = k
     return
 
+  # Find the stock code associated with a given name
   def name2code(self, stock_name):
     try:
       return self.name_dict[stock_name]
     except:
       return None
 
+  # Get the preferred name of a specific stock
   def code2name(self, stock_code):
     try:
       return self.listing[stock_code]
     except:
       return ''
   
+  # Cache the historical data from Quandl for a list of stocks as a dict of Pandas `dataframes`
   def cacheFromQuandl(self, stock_codes, start_date, end_date=None):
     if end_date == None:
       end_date = date.today()
     if isinstance(stock_codes,list):
       for s in stock_codes:
-        qs = 'HKEX/' + s.rstrip('.hk').zfill(5)
-        df = pdr.DataReader(qs, 'quandl', start_date, end_date)
+        qs = 'HKEX/' + s.rstrip('.hk').zfill(5) # Translate the stock code from Yahoo to Quandl format
+        df = pdr.DataReader(qs, 'quandl', start_date, end_date) # Fetch data from Quandl using pandas-datareader
         self.dataframes[s] = df.copy()
         stock_name = self.code2name(s)
         print(f'{stock_name}({s}):')
